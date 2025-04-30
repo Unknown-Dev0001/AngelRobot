c9eb3b1a3430cf693178b2a007248e5be48f1421
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-04-30 16:15:06 +0530

    Update config.py

--- AngelRobot/config.py
+++ AngelRobot/config.py
@@ -22,7 +22,7 @@ class Config(object):
 
     TIME_API_KEY = "UGY3YRGBQPLZ"  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 5242142514 # User id of your telegram account (Must be integer)
+    OWNER_ID = 7500269454 # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
